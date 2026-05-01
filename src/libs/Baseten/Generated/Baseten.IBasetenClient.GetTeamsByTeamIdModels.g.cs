43208c60bf85ff84be15620e7745482dc4b3944e
Konstantin S. <havendv@gmail.com>
2026-05-01 23:21:25 +0400

    feat: Updated OpenAPI spec (#34)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

---
+++ src/libs/Baseten/Generated/Baseten.IBasetenClient.GetTeamsByTeamIdModels.g.cs
@@ -0,0 +1,24 @@
+#nullable enable
+
+namespace Baseten
+{
+    public partial interface IBasetenClient
+    {
+        /// <summary>
+        /// Gets all models
+        /// </summary>
+        /// <param name="teamId"></param>
+        /// <param name="requestOptions">Per-request overrides such as headers, query parameters, timeout, retries, and response buffering.</param>
+        /// <param name="cancellationToken">The token to cancel the operation with</param>
+        /// <exception cref="global::Baseten.ApiException"></exception>
+        /// <remarks>
+        /// curl --request GET \<br/>
+        /// --url https://api.baseten.co/v1/teams/{team_id}/models \<br/>
+        /// --header "Authorization: Api-Key $BASETEN_API_KEY"
+        /// </remarks>
+        global::System.Threading.Tasks.Task<global::Baseten.ModelsV1> GetTeamsByTeamIdModelsAsync(
+            string teamId,
+            global::Baseten.AutoSDKRequestOptions? requestOptions = default,
+            global::System.Threading.CancellationToken cancellationToken = default);
+    }
+}
